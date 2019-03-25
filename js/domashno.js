1f1159ec948c0c5bfcfaf8a901cdc397ae39e517
slavenapresley <slavena_ilieva90@abv.bg>
2019-03-25 23:57:30 +0200

    js function convector

---
+++ js/domashno.js
@@ -0,0 +1,17 @@
+function myConvect() {
+    document.getElementById("field4").value =
+        document.getElementById("field3").value * 1.95583;
+}
+
+function myFunction() {
+    document.getElementById("field2").value =
+        document.getElementById("field1").value * 0.511292;
+}
+
+function myLimits() {
+    if
+      (document.getElementById("field5").value < 18) 
+      alert("You haven`t enough years")
+    else
+     alert("You can go to play");
+    }
