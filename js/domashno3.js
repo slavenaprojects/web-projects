ceefeca3443e1d16f56f81bc5deef6f6e8a29857
slavenapresley <slavena_ilieva90@abv.bg>
2019-04-07 22:59:56 +0300

    domashno Date

---
+++ js/domashno3.js
@@ -0,0 +1,31 @@
+//task 1
+function myWay(date) {
+  	console.log(date instanceof Date && !isNaN(date.valueOf())) 
+		if(date instanceof Date && !isNaN(date.valueOf())) {
+		return "Good!";
+		} else { 
+		return "Sorry!";}
+} 
+
+//task 2
+var d = new Date();
+var months = ["January","February","March","April","May","June","July","August","September",
+"October","November","December"];
+document.getElementById("demo").innerHTML = months[d.getMonth()];
+
+// task 3
+var d = new Date();
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+document.getElementById("demo").innerHTML = days[d.getDay()];
+
+// task 4
+var myBirthday, today, bday, diff, days;
+myBirthday = [30,6]; // 6th of February
+today = new Date();
+bday = new Date(today.getFullYear(),myBirthday[1]-1,myBirthday[0]);
+if( today.getTime() > bday.getTime()) {
+    bday.setFullYear(bday.getFullYear()+1);
+}
+diff = bday.getTime()-today.getTime();
+days = Math.floor(diff/(1000*60*60*24));
+alert(days+" days until Slavena's birthday!");
